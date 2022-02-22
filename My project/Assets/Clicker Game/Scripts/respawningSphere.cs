c35a90af089e6a922025affe1c26baacf08488fd
RameetB <71399669+RameetB@users.noreply.github.com>
2022-02-21 17:06:05 -0700

    grgrrgr
    so we start with a sphere loaded infinite spawning on start removed

--- My project/Assets/Clicker Game/Scripts/respawningSphere.cs
+++ My project/Assets/Clicker Game/Scripts/respawningSphere.cs
@@ -5,33 +5,33 @@ using UnityEngine;
 public class respawningSphere : MonoBehaviour
 {
     public GameObject spawnPrefab;
-    public float xPos = 0f, yPos = 0f, zPos = 2f;
-    public int hp = 5;
-    void Start() {
-        if (spawnPrefab != null) {
-        Instantiate(spawnPrefab, Vector3.zero, Quaternion.identity);
-        } else {
-            Debug.LogError("spawnPrefab not selected.");
-        }
-    }
+    public scoreManager scoreManager;
 
+    public float xPos = 0f, yPos = 0f, zPos = 2f;
+    public int hp = 5, points = 5;
+
+    public float sizeScale = .2f;    
     void OnMouseDown () {
         hp--;
-        transform.localScale += new Vector3(.2f,.2f,.2f);
+
+        transform.localScale += Vector3.one * sizeScale;
+
         if (hp == 0) {
+
             Destroy(gameObject);
-            if (spawnPrefab != null) {
-                spawnNew();
-            } else {
-                Debug.LogError("Cannot respawn no prefab loaded.");
-            }
+
+            scoreManager.increaseScore(points);
+
+            spawnNew();
         }
     }
-
     void spawnNew() {
         
         xPos = Random.Range(-12.0f, 12.0f);
         yPos = Random.Range(-5.2f, 7.3f);
+        int newHp = Random.Range(0,11);
+        hp = newHp;
+        points = newHp;
         Vector3 spawnPos = new Vector3(xPos, yPos, zPos);
         Instantiate(spawnPrefab, spawnPos, Quaternion.identity);
     }
